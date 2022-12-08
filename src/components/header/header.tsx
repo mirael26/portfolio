872b7f49f372e9fef9441f70c59c7b3fe7ae30e3
Elmira Ilyasova <capricorn_26@mail.ru>
2022-12-08 15:27:35 +0700

    Add badge to header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom';
+import { AppUrl } from '../../consts';
+import './header.scss';
+
 const Header = () => (
-  <></>
+  <div className="header">
+    <Link to={AppUrl.Main} className="header__badge-link">
+      <div className="header__badge">
+        Эльмира Ильясова
+        <span className="header__badge-subtitle">Frontend developer</span>
+      </div>
+    </Link>
+  </div>
 );
 
 export default Header;
