bebf846de807102823540f1c64e62db21ce063c6
Elmira Ilyasova <capricorn_26@mail.ru>
2022-12-09 00:46:22 +0700

    Edit nav animation

--- src/components/header/main-nav/main-nav.tsx
+++ src/components/header/main-nav/main-nav.tsx
@@ -6,31 +6,31 @@ const MainNav = () => (
   <ul className="main-nav">
     <li className="main-nav__item">
       <Link to={AppUrl.About} className="main-nav__link main-nav__link--about">
-        Обо мне
+      <span className='main-nav__text'>Обо мне</span>
       </Link>
     </li>
     
     <li className="main-nav__item">
       <Link to={AppUrl.Skills} className="main-nav__link main-nav__link--skills">
-        Навыки
+        <span className='main-nav__text'>Навыки</span>
       </Link>
     </li>
       
     <li className="main-nav__item">
       <Link to={AppUrl.Education} className="main-nav__link main-nav__link--education">
-        Обучение
+      <span className='main-nav__text'>Обучение</span>
       </Link>
     </li>
       
     <li className="main-nav__item">
       <Link to={AppUrl.Experience} className="main-nav__link main-nav__link--experience">
-        Опыт работы
+      <span className='main-nav__text'>Опыт работы</span>
       </Link>
     </li>
       
     <li className="main-nav__item">
       <Link to={AppUrl.Portfolio} className="main-nav__link main-nav__link--portfolio">
-        Портфолио
+      <span className='main-nav__text'>Портфолио</span>
       </Link>
     </li>
   </ul>
