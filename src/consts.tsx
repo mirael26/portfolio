4bf7505c996c16f537f534b8539c79094669cfc4
Elmira Ilyasova <capricorn_26@mail.ru>
2023-01-02 12:09:06 +0700

    Add sertificates and move edu info to consts

--- src/consts.tsx
+++ src/consts.tsx
@@ -6,3 +6,38 @@ export const AppUrl = {
   Experience: '/experience',
   Portfolio: '/portfolio',
 } as const;
+
+
+
+export const educationInfo = [
+  {
+    year: '2008-2053',
+    title: 'Маркетинг (высшее)',
+    institution: 'СибУПК'
+  },
+  {
+    year: '2019',
+    title: 'HTML и CSS, уровень 1',
+    institution: 'HTML Academy'
+  },
+  {
+    year: '2020',
+    title: 'HTML и CSS, уровень 2',
+    institution: 'HTML Academy'
+  },
+  {
+    year: '2020',
+    title: 'JavaScript. Профессиональная разработка веб-интерфейсов',
+    institution: 'HTML Academy'
+  },
+  {
+    year: '2020',
+    title: 'JavaScript. Архитектура клиентских приложений',
+    institution: 'HTML Academy'
+  },
+  {
+    year: '2020',
+    title: 'React. Разработка сложных клиентских приложений',
+    institution: 'HTML Academy'
+  },
+];
